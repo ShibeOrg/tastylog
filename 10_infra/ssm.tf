bfd35c16c90a8867ac0849fe71e32b634c84cc70
kuraudo <kuraudo@example.com>
2025-12-13 11:58:44 +0900

    修正

--- 10_infra/ssm.tf
+++ 10_infra/ssm.tf
@@ -5,6 +5,9 @@ resource "aws_ssm_parameter" "host" {
   name  = "/${var.project}/${var.environment}/app/MYSQL_HOST"
   type  = "String"
   value = aws_db_instance.mysql_standalone.address
+  allowed_pattern = ""
+  description     = ""
+  key_id          = ""
 }
 
 resource "aws_ssm_parameter" "port" {
@@ -12,6 +15,9 @@ resource "aws_ssm_parameter" "port" {
   type  = "String"
   value = aws_db_instance.mysql_standalone.port
   # value = "3306"
+  allowed_pattern = ""
+  description     = ""
+  key_id          = ""
 }
 
 resource "aws_ssm_parameter" "database" {
@@ -19,6 +25,9 @@ resource "aws_ssm_parameter" "database" {
   type  = "String"
   value = aws_db_instance.mysql_standalone.name
   # value = "tastylog"
+  allowed_pattern = ""
+  description     = ""
+  key_id          = ""
 }
 
 resource "aws_ssm_parameter" "username" {
